3f29e1b47a0dd6db73d42429175e11683df7c1b5
dhane31 <dhanegm731@gmail.com>
2021-04-05 18:23:02 +0900

    Add scan QR code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
     );
@@ -24,6 +24,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String qrCode = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,13 +35,33 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text(
+              '$qrCode',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
             ElevatedButton(
               child: Text('Start QRCode Scan'),
-              onPressed: () {},
+              onPressed: () => scanQrCode(),
             ),
+            SizedBox(height: 10),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> scanQrCode() async {
+    final qrCode = await FlutterBarcodeScanner.scanBarcode(
+      '#EB394B',
+      'Cancel',
+      true,
+      ScanMode.QR,
+    );
+    if (!mounted) return;
+
+    setState(() {
+      this.qrCode = qrCode;
+    });
   }
 }
